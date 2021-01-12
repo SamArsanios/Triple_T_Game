c15614bfcabf940811a561b8f0b2c924e920d120
[sam arsanios] <samson.kibrom@hotmail.com>
2021-01-12 18:36:51 +0300

    Remove logic from code

--- bin/main.rb
+++ bin/main.rb
@@ -5,28 +5,20 @@ puts '*  Triple-T-Game   *'
 puts '********************'
 
 # Create the board, playing ground
-puts 'Players make their moves in the board'
 
 # Create a game loop
 
 # Welcoming message
-puts 'Welcome to this game!'
 
 # Asking names of players
-puts 'Player-1, please enter your name:'
-player1 = gets.chomp
-
-puts 'Player-2, please enter your name:'
-player2 = gets.chomp
 
 # Ready message
-puts "Welcome Player-1 - #{player1} and Player-2 - #{player2}. You are ready to start the game!"
 
 # Display the game board
 
 # Inform players about instructions of the game(place x symbol or o symbol on the board)
 
-# Player-1 always starts first
+# Player_1 always starts first
 
 # Inform Player-1 if it is an invalid move and ask to make a valid move
 
@@ -34,9 +26,9 @@ puts "Welcome Player-1 - #{player1} and Player-2 - #{player2}. You are ready to 
 
 # Ask Player-2 to make a valid move
 
-# Inform Player-2 if it is an invalid move and ask to make a valid move
+# Inform Player_2 if it is an invalid move and ask to make a valid move
 
-# Continue the loop until one player wins
+# Continue the loop until one player wins or the game ends with a draw
 
 # Winner is declared
 
