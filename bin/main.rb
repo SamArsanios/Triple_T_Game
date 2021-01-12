fc64ec5f9aa05ef590777942f9956058088e6d9b
[sam arsanios] <samson.kibrom@hotmail.com>
2021-01-12 16:29:05 +0300

    Add welcome screen messages

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,15 @@
 #!/usr/bin/env ruby
 
-print 'Hello World!'
+puts "********************"
+puts "*  Triple-T-Game   *"
+puts "********************"
+
+puts "Welcome to this game!"
+
+puts "Player-1, please enter your name:"
+player1 = gets.chomp
+
+puts "Player-2, please enter your name:"
+player2 = gets.chomp
+
+puts "Welcome Player-1 - #{player1} and Player-2 - #{player2}. You are ready to start the game!"
