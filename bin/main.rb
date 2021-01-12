9806fb5a4f49fbc6b8c411fac5490e6385bee69f
[sam arsanios] <samson.kibrom@hotmail.com>
2021-01-12 16:45:55 +0300

    Add Game Flow

--- bin/main.rb
+++ bin/main.rb
@@ -13,3 +13,14 @@ puts "Player-2, please enter your name:"
 player2 = gets.chomp
 
 puts "Welcome Player-1 - #{player1} and Player-2 - #{player2}. You are ready to start the game!"
+
+#Create the board, playing ground
+puts "Players make their moves in the board"
+
+#create the loop for the trials
+loop do
+puts "Player-1 -> #{player1} make your move"
+#check if player 1 wins, if not let player 2 make a move
+
+puts "Player-2 -> #{player2} make your move"
+end
