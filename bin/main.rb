af31a61e7e1bd49bd011e4d2396e06af76d54b94
[sam arsanios] <samson.kibrom@hotmail.com>
2021-01-12 16:55:31 +0300

    Fix linter errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,42 +1,41 @@
 #!/usr/bin/env ruby
 
-puts "********************"
-puts "*  Triple-T-Game   *"
-puts "********************"
+puts '********************'
+puts '*  Triple-T-Game   *'
+puts '********************'
 
-puts "Welcome to this game!"
+puts 'Welcome to this game!'
 
-puts "Player-1, please enter your name:"
+puts 'Player-1, please enter your name:'
 player1 = gets.chomp
 
-puts "Player-2, please enter your name:"
+puts 'Player-2, please enter your name:'
 player2 = gets.chomp
 
 puts "Welcome Player-1 - #{player1} and Player-2 - #{player2}. You are ready to start the game!"
 
-#Create the board, playing ground
-puts "Players make their moves in the board"
+# Create the board, playing ground
+puts 'Players make their moves in the board'
 
-#create the loop for the trials
+# create the loop for the trials
 gameover = false
-winner = ""
+winner = ''
 loop do
-puts "Player-1 -> #{player1} make your move"
-#check if player 1 wins, if not let player 2 make a move
+  puts "Player-1 -> #{player1} make your move"
+  # check if player 1 wins, if not let player 2 make a move
 
-puts "Player-2 -> #{player2} make your move"
-break if gameover = true
-winner = "#{player1 or player2}"
+  puts "Player-2 -> #{player2} make your move"
+  break if gameover == true
+
+  winner = (player1 or player2).to_s
 end
 
-#Winner is declared
+# Winner is declared
 puts "The winner is #{player1 or player2}, Congratulations Mate!"
 
-puts "Do you want to play again?"
-answer = gets.chomp.upcase.to_s
+puts 'Do you want to play again?'
+answer = gets.chomp
 
-#if Yes, then continue if not then quit the game
+puts "Your answer is #{answer}"
 
-
-
-
+# if Yes, then continue if not then quit the game
