9d02b18aa5b97a13e9ce13cd2818f3d21c50537c
[sam arsanios] <samson.kibrom@hotmail.com>
2021-01-12 13:45:21 +0300

    Add player prompts

---
+++ bin/main.rb
@@ -0,0 +1,14 @@
+#! \Users\aghapy\Desktop\microverse\ruby\Triple_T_Game\bin\main.rb
+puts "****************************"
+puts "Welcome to the Triple-T-Game"
+puts "****************************"
+
+puts "Kindly, Player 1, enter your name here:"
+player1 = gets.chomp.to_s
+
+puts "Kindly, Player 2, enter your name here:"
+player2 = gets.chomp.to_s
+
+loop do
+
+end
