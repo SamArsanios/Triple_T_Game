53f0df4b45a819d50af40f8f2af55ca831f26965
[sam arsanios] <samson.kibrom@hotmail.com>
2021-01-12 16:46:37 +0300

    Add Game flow

--- bin/main.rb
+++ bin/main.rb
@@ -18,9 +18,25 @@ puts "Welcome Player-1 - #{player1} and Player-2 - #{player2}. You are ready to 
 puts "Players make their moves in the board"
 
 #create the loop for the trials
+gameover = false
+winner = ""
 loop do
 puts "Player-1 -> #{player1} make your move"
 #check if player 1 wins, if not let player 2 make a move
 
 puts "Player-2 -> #{player2} make your move"
+break if gameover = true
+winner = "#{player1 or player2}"
 end
+
+#Winner is declared
+puts "The winner is #{player1 or player2}, Congratulations Mate!"
+
+puts "Do you want to play again?"
+answer = gets.chomp.upcase.to_s
+
+#if Yes, then continue if not then quit the game
+
+
+
+
