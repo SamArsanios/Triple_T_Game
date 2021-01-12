0fa5223b0aa395e3e2ad932fc38108beaf61d43c
Necmi Gunduz <necmigunduz>
2021-01-12 17:43:46 +0300

    Complete the pseudocode of the game

--- bin/main.rb
+++ bin/main.rb
@@ -22,13 +22,21 @@ player2 = gets.chomp
 # Ready message
 puts "Welcome Player-1 - #{player1} and Player-2 - #{player2}. You are ready to start the game!"
 
-# Player-1 always starts first
+# Display the game board
+
+# Inform players about instructions of the game(place x symbol or o symbol on the board)
+
+# Player-1 always starts first 
 
 puts "#{player-1} starts first. Make the first move."
 
+# Inform Player-1 if it is an invalid move and ask to make a valid move
+
 # Check if player 1 wins, if not let player 2 make a move
 
-puts "Player-2 -> #{player2} make your move"
+# Ask Player-2 to make a valid move
+
+# Inform Player-2 if it is an invalid move and ask to make a valid move
 
 # Continue the loop until one player wins
 
@@ -36,4 +44,6 @@ puts "Player-2 -> #{player2} make your move"
 
 # Ask players if they would like to continue playing(y/n)
 
-# If one of the players would like to quit the game, declare a thank you message
+# If one of the players would like to quit the game, declare a thank you message and end the game
+
+# Otherwise, do not break the loop
