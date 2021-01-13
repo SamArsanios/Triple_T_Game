2b60cbaa3466d8351be1b100e86325efda28dee1
[sam arsanios] <samson.kibrom@hotmail.com>
2021-01-13 17:16:50 +0300

    Add the messages

--- lib/game_play.rb
+++ lib/game_play.rb
@@ -50,4 +50,24 @@ module GamePlay
         ---------
         #{grid_array[6]} | #{grid_array[7]} | #{grid_array[8]}\n \n"
     end
+
+    def pick_number(player_name)
+        puts "#{player_name}, Please pick an available number"
+    end
+
+    def pick_not_valid
+        puts "Your pick is not valid, please make another pick"
+    end
+
+    def already_picked_message
+        puts "This number is already pick, please make another pick."
+    end
+
+    def declare_winner
+        puts "Congratulations Dude!!!!, You are the winner"
+    end
+
+    def game_tie
+        puts "The game is a Tie!, Play Again!"
+    end
 end
