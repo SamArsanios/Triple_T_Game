a9b806bc56f4039184cf931c5904f84fe186d5f5
Necmi Gunduz <necmigunduz>
2021-01-13 18:30:10 +0300

    Start game.rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,52 +1,30 @@
 #!/usr/bin/env ruby
+require_relative '../lib/player.rb'
+require_relative '../lib/game.rb'
+require_relative '../lib/gameboard.rb'
+require_relative '../lib/game_play.rb'
 
+# Initialize the game
+player1 = Players.new('O')
+player2 = Players.new('X')
 
+valid_player_pick = 0
 
+game = Game.new(player1,player2)
 
-puts 'Player One: Enter your name please\n'
-player1 = gets.chomp
-puts "Welcome #{player1}"
+puts GamePlay.welcome
 
-puts 'Player Two: Enter your name please\n\n'
-player2 = gets.chomp
-puts "Welcome #{player2}"
+puts GamePlay.instructions
 
-puts 'The Game is starting! Ready, steady, and go!\n'
+initial_player = proc do |player, number|
+    puts GamePlay.player_names(number)
+    player.name = gets.chomp.capitalize
+    while player.name.empty?
+        puts Game.validate_name
+        player.name = gets.chomp.capitalize
+    end
+    puts GamePlay.assign_totem(player)
+end
 
-# Display the game board and
-
-# winner = false
-# # Inform Player-1 if it is an invalid move and ask to make a valid move
-# puts 'Request Player1 to make move'
-# puts 'Player1 please make your move by selecting a number between 1 - 9'
-# player1_move = gets.chomp
-# puts "Player1 one selected the move of #{player1_move}."
-
-# # Check if Player-1 makes a valid move. If not, give a warning and repeat the move selection.
-# puts "Player1' move is invalid. Make a valid move by picking a number between 1 - 9."
-
-# # If Player1's move is valid, then check if Player1 wins, or the game ends with a draw.
-# puts 'Player1 wins!'
-# puts 'The game ended with draw.'
-
-# puts "#{Player2}, make your move!"
-# player2_move = gets.chomp
-# puts "Player2 selected the move of #{player2_move}."
-
-# # If Player2's move is valid, then check if Player2 wins, or the game ends with a draw.
-# puts 'Player2 wins!'
-# puts 'The game ended with draw.'
-
-# # Continue the loop until one player wins or the game ends with a draw
-# # Winner is declared or a Tie is declared
-# break if winner == true
-
-# puts 'Congratulations you have won the game'
-
-# # Ask players if they would like to continue playing(y/n)
-# puts 'Would you like to play again?'
-# play_again = gets.chomp
-# puts "you have chosen #{play_again}"
-
-# # If one of the players would like to quit the game, declare a thank you message and end the game
-# puts 'Thanks for playing!'
+initial_player.call(player1, 1)
+initial_player.call(player2, 2)
