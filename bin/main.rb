e389333f2795c4a0e04129e075d81c8ffd0f4613
[sam arsanios] <samson.kibrom@hotmail.com>
2021-01-13 18:33:47 +0300

    Add complete main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -28,3 +28,78 @@ end
 
 initial_player.call(player1, 1)
 initial_player.call(player2, 2)
+
+validate_pick = proc do |pick|
+  valid_pick = pick
+  until board.pick_valid?(pick) && board.not_picked?(pick)
+    if !board.pick_valid?(pick)
+      puts game.pick_not_valid
+      pick = gets.chomp.to_i
+    elsif !board.not_picked?(pick)
+      puts game.already_picked
+      pick = gets.chomp.to_i
+    end
+  end
+  valid_pick = pick
+end
+turn = proc do |player|
+  puts game.pick_number(player.name)
+  pick = gets.chomp.to_i
+  validate_pick.call(pick)
+  pick = valid_pick
+  player.picks_array << pick
+  board.update_grid(pick, player.totem)
+  puts game.display_grid(board.grid)
+end
+loop do
+  turn.call(player1)
+  break if player1.winner? || board.grid_filled?
+  turn.call(player2)
+  break if player2.winner? || board.grid_filled?
+end
+# end of the game
+if player1.winner?
+  puts game.the_winner(player1.name)
+elsif player2.winner?
+  puts game.the_winner(player2.name)
+else
+  puts game.draw
+end
+
+
+# validate_pick = proc do |pick|
+#     valid_pick = pick
+#     until board.pick_valid?(pick) && board.not_picked?(pick)
+#       if !board.pick_valid?(pick)
+#         puts game.pick_not_valid
+#         pick = gets.chomp.to_i
+#       elsif !board.not_picked?(pick)
+#         puts game.already_picked
+#         pick = gets.chomp.to_i
+#       end
+#     end
+#     valid_pick = pick
+#   end
+#   turn = proc do |player|
+#     puts game.pick_number(player.name)
+#     pick = gets.chomp.to_i
+#     validate_pick.call(pick)
+#     pick = valid_pick
+#     player.picks_array << pick
+#     board.update_grid(pick, player.totem)
+#     puts game.display_grid(board.grid)
+#   end
+#   loop do
+#     turn.call(player1)
+#     break if player1.winner? || board.grid_filled?
+#     turn.call(player2)
+#     break if player2.winner? || board.grid_filled?
+#   end
+#   # end of the game
+#   if player1.winner?
+#     puts game.the_winner(player1.name)
+#   elsif player2.winner?
+#     puts game.the_winner(player2.name)
+#   else
+#     puts game.draw
+#   end
