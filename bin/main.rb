8bf7a83d76781cded791a902978b237593090785
Necmi Gunduz <necmigunduz>
2021-01-13 16:08:47 +0300

    Add player.rb

--- bin/main.rb
+++ bin/main.rb
@@ -6,10 +6,9 @@ puts '|                 Welcome to Triple-T-Game                 |'
 puts '|                                                          |'
 puts '------------------------------------------------------------'
 
-# Welcoming message
-puts 'Hi there, welcome to Triple T, Ready!'
+puts 'Hi there, welcome to Triple T, Ready!\n\n'
 
-# The guide for players
+puts 'Game instructions\n'
 puts 'The players will be provided a board to make their moves on as shown below.'
 
 puts ' 1 | 2 | 3 '
@@ -28,62 +27,66 @@ puts 'For winning the game, one player should have one of the following combinat
 puts 'The winning combinations are 123, 456, 789, 147, 258, 369, 159, and 357.'
 puts 'After completing their moves, if no player cat get one of the winning combinations, it is a draw.'
 puts 'After the game ended up with a winner, or a draw, players are asked whether they would like to play again.'
-puts 'If their answer is yes (y), then the game restarts, otherwise, the game goes into sleeping.'
+puts 'If their answer is yes (y), then the game restarts, otherwise, the game goes into sleeping.\n\n'
 
-# Asking names of players
-puts 'Player One: Enter your name please'
+puts 'Player One: Enter your name please\n'
 player1 = gets.chomp
 puts "Welcome #{player1}"
 
-puts 'Player Two: Enter your name please'
+puts 'Player Two: Enter your name please\n\n'
 player2 = gets.chomp
 puts "Welcome #{player2}"
 
-# Ready message
-puts 'The Game is starting! Ready, steady, and go!'
-# Inform players about instructions of the game(place x symbol or o symbol on the board)
-puts 'Game instructions'
+puts 'The Game is starting! Ready, steady, and go!\n'
 
 # Display the game board and
-puts 'Display the board'
-puts '[ 1 | 2 | 3 ]'
-puts '-------------'
-puts '[ 4 | 5 | 6 ]'
-puts '-------------'
-puts '[ 7 | 8 | 9 ]'
+class GameBoard
+    attr_accessor :grid
+    def initialize(grid= [1,2,3,4,5,6,7,8,9])
+        @grid = grid
+    end
 
-winner = false
-# Inform Player-1 if it is an invalid move and ask to make a valid move
-puts 'Request Player1 to make move'
-puts 'Player1 please make your move by selecting a number between 1 - 9'
-player1_move = gets.chomp
-puts "Player1 one selected the move of #{player1_move}."
+    def display_grid(grid_arr)
+        "\n #{grid_arr[0]} | #{grid_arr[1]} | #{grid_arr[2]} 
+        ----------
+        #{grid_array[3]} | #{grid_array[4]} | #{grid_array[5]}
+        ---------
+        #{grid_array[6]} | #{grid_array[7]} | #{grid_array[8]}\n \n"
+    end
+end
+    a = GameBoard.display_grid (grid[])
+# winner = false
+# # Inform Player-1 if it is an invalid move and ask to make a valid move
+# puts 'Request Player1 to make move'
+# puts 'Player1 please make your move by selecting a number between 1 - 9'
+# player1_move = gets.chomp
+# puts "Player1 one selected the move of #{player1_move}."
 
-# Check if Player-1 makes a valid move. If not, give a warning and repeat the move selection.
-puts "Player1' move is invalid. Make a valid move by picking a number between 1 - 9."
+# # Check if Player-1 makes a valid move. If not, give a warning and repeat the move selection.
+# puts "Player1' move is invalid. Make a valid move by picking a number between 1 - 9."
 
-# If Player1's move is valid, then check if Player1 wins, or the game ends with a draw.
-puts 'Player1 wins!'
-puts 'The game ended with draw.'
+# # If Player1's move is valid, then check if Player1 wins, or the game ends with a draw.
+# puts 'Player1 wins!'
+# puts 'The game ended with draw.'
 
-puts "#{Player2}, make your move!"
-player2_move = gets.chomp
-puts "Player2 selected the move of #{player2_move}."
+# puts "#{Player2}, make your move!"
+# player2_move = gets.chomp
+# puts "Player2 selected the move of #{player2_move}."
 
-# If Player2's move is valid, then check if Player2 wins, or the game ends with a draw.
-puts 'Player2 wins!'
-puts 'The game ended with draw.'
+# # If Player2's move is valid, then check if Player2 wins, or the game ends with a draw.
+# puts 'Player2 wins!'
+# puts 'The game ended with draw.'
 
-# Continue the loop until one player wins or the game ends with a draw
-# Winner is declared or a Tie is declared
-break if winner == true
+# # Continue the loop until one player wins or the game ends with a draw
+# # Winner is declared or a Tie is declared
+# break if winner == true
 
-puts 'Congratulations you have won the game'
+# puts 'Congratulations you have won the game'
 
-# Ask players if they would like to continue playing(y/n)
-puts 'Would you like to play again?'
-play_again = gets.chomp
-puts "you have chosen #{play_again}"
+# # Ask players if they would like to continue playing(y/n)
+# puts 'Would you like to play again?'
+# play_again = gets.chomp
+# puts "you have chosen #{play_again}"
 
-# If one of the players would like to quit the game, declare a thank you message and end the game
-puts 'Thanks for playing!'
+# # If one of the players would like to quit the game, declare a thank you message and end the game
+# puts 'Thanks for playing!'
