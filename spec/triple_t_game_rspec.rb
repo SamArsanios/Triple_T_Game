3d38375288e5fc62bc7401bdf12faeea36e08995
[sam arsanios] <samson.kibrom@hotmail.com>
2021-01-20 12:00:41 +0300

    Modify player names test case

--- spec/triple_t_game_rspec.rb
+++ spec/triple_t_game_rspec.rb
@@ -7,15 +7,9 @@ describe GamePlay do
     let(:new_GamePlay) { Class.new { extend GamePlay } }
     describe "#player_names" do
         it "returns player number" do 
-            expect(new_GamePlay.player_names(1)).to eql("Player1, Please enter your name")
+            new_player = 2
+            expect(new_GamePlay.player_names(new_player)).to eql("Player#{new_player}, Please enter your name")
         end
     end
-
-    describe "#assign_totem" do
-        it "returns assign totem" do 
-            expect(new_GamePlay.assign_totem(1)).to eql("")
-        end
-    end
-
 end
 
