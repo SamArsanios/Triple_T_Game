85873e8fadf6a3b74ff0b05380c0de796f28206e
Alamgir <ourandoc@gmail.com>
2021-01-20 16:20:49 +0500

    Fix linters

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -98,4 +98,4 @@ RSpec.configure do |config|
   # # as the one that triggered the failure.
   # Kernel.srand config.seed
 end
-# rubocop:disable Style/BlockComments
+# rubocop:enable Style/BlockComments
