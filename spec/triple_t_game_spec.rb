f7fde0ed7bfbc04a451c8fda6d0e0b4103c07845
[sam arsanios] <samson.kibrom@hotmail.com>
2021-01-20 17:12:10 +0300

    Add grid_all test case, separate negative cases from positive ones

--- spec/triple_t_game_spec.rb
+++ spec/triple_t_game_spec.rb
@@ -6,9 +6,13 @@ require_relative '../lib/player.rb'
 describe GamePlay do
   let(:new_GamePlay) { Class.new { extend GamePlay } }
   describe '#player_names' do
-    it 'returns player number' do
+    it 'returns the number of the player' do
       new_player = 2
       expect(new_GamePlay.player_names(new_player)).to eql("Player#{new_player}, Please enter your name")
+    end
+
+    it 'returns false if the player number is invalid' do
+      new_player = 2
       expect(new_GamePlay.player_names(new_player)).to_not eql('Player3, Please enter your name')
     end
   end
@@ -19,23 +23,47 @@ describe GameBoard do
   describe '#validate_pick?' do
     it 'checks for a valid pick in the board' do
       expect(new_GameBoard.validate_pick?(2)).to eql(true)
+    end
+
+    it 'checks for an invalid pick in the board' do
       expect(new_GameBoard.validate_pick?(10)).to_not eql(true)
     end
   end
 
   describe '#already_picked' do
-    it 'checks for a valid pick in the board' do
+    it 'checks for already taken pick in the board' do
       expect(new_GameBoard.already_picked(2)).to eql(true)
+    end
+
+    it 'checks for invalid pick in the board' do
       expect(new_GameBoard.already_picked(10)).to_not eql(true)
     end
   end
 
   describe '#replace_grid' do
-    it 'replaces a number in grid with the totem value' do
+    it 'returns true if the value is X' do
       expect(new_GameBoard.replace_grid(2, 'X')).to eql([1, 'X', 3, 4, 5, 6, 7, 8, 9])
-      expect(new_GameBoard.replace_grid(2, 'O')).to_not eql([1, 'X', 3, 4, 5, 6, 7, 8, 9])
+    end
+
+    it 'returns true if the value is O' do
+      expect(new_GameBoard.replace_grid(2, 'O')).to eql([1, 'O', 3, 4, 5, 6, 7, 8, 9])
+    end
+
+    it 'returns true if choice is invalid' do
+      expect(new_GameBoard.replace_grid(11, 'O')).to_not eql([1, 'O', 3, 4, 5, 6, 7, 8, 9])
     end
   end
+
+  describe "#fill_grid?" do
+  it "checks for string" do
+   expect(new_GameBoard.fill_grid?).to eql(false) 
+  end
+
+  it "return true for no string value" do
+    expect(new_GameBoard.fill_grid?).to_not eql(true) 
+   end
+
+end
 end
 
 describe Players do
@@ -48,3 +76,5 @@ describe Players do
     end
   end
 end
+
+
